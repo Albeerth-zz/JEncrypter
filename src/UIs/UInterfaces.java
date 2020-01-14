f42ec939af945c62a55532fde38964ccdb971dc8
Alberto Galdámez <aa.galdammez@gmail.com>
2020-01-13 22:23:05 -0600

    Working on UInterfaces, i have added 'System.exit(1)' to exit.

--- src/UIs/UInterfaces.java
+++ src/UIs/UInterfaces.java
@@ -3,16 +3,18 @@ package UIs;
 public class UInterfaces extends Important.InputScann {
 
     public static void PrincipalMenu(){
-        System.out.println("JENCRYPTER MENU");
+        System.out.println("J-ENCRYPTER MENU");
         System.out.println("[1] Encrypt a Message");
         System.out.println("[2] Decrypt a Message");
         System.out.println("[3] Exit");
         switch (ReadInt()){
             case 1:
+                EncryptMenu();
                 break;
             case 2:
                 break;
             case 3:
+                System.exit(1);
                 break;
             default:
                 System.out.println("That's not an option.");
@@ -20,7 +22,17 @@ public class UInterfaces extends Important.InputScann {
     }
 
     public static void EncryptMenu(){
-
+        System.out.println("ENCRYPT MENU");
+        System.out.println("[1] Encrypt a Message");
+        System.out.println("[2] Go Back");
+        switch (ReadInt()){
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
     }
     public static void DecryptMenu() {
 
